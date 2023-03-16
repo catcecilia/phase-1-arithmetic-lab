45e8d31cf620a905e586253564c461be2f35b4ef
Phuong-Cat Ngo <phuongcatngo@gmail.com>
2023-03-15 22:53:02 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 2;
+let num2 = 31;
+let num3 = 25;
+let num4 = 7;
+let array = [20, 1, 0, 5];
+let max = 0;
 
-// Write your code here
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random()+2);
+
+let mod = num3%num4;
+
+for (let i = 0; i < array.length; i++){
+    if (max<array[i]){
+        max = array[i]
+    }
+} 
